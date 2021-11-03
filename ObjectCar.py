52eeade9d2da3cc61239a2afb73e1b10a2cf43b1
William Plante <plaw2901@usherbrooke.ca>
2021-11-03 15:56:41 -0400

    Blender plante, papa revient du depanneur svp

---
+++ ObjectCar.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+class ObjectCar :
+    
+    def __init__(self, ob):
+        self.ob=ob
+        self.facingAngle =  ob.rotation_euler[2]
+        self.positionXYZ = [ob.location[0],ob.location[1],ob.location[2]]
+        
+    def updateOb(self):
+        self.ob.location[0] =self.positionXYZ[0] 
+        self.ob.location[1] =self.positionXYZ[1] 
+        self.ob.location[2] =self.positionXYZ[2]
+        self.ob.rotation_euler[2] = self.facingAngle 
+        self.updateSensorPos()
+        
+    def updateSensorPos(self):
+        distanceX = 13.57
+        distanceXDist = 14.82
+        distanceY = 1.8
+
+        angle1 = np.tan(distanceY/distanceX)
+        angle2 = np.tan(2 * distanceY/distanceX)
+        self.lineSensor.append([(self.ob.location[0]+distanceX*np.cos(self.ob.rotation_euler[2]))*np.cos(angle2),(self.ob.location[0]+distanceX*np.sin(self.ob.rotation_euler[2]))*np.sin(angle2)])
+        self.lineSensor.append([(self.ob.location[0]+distanceX*np.cos(self.ob.rotation_euler[2]))*np.cos(angle1),(self.ob.location[0]+distanceX*np.sin(self.ob.rotation_euler[2]))*np.sin(angle1)])
+        self.lineSensor.append([self.ob.location[0]+distanceX*np.cos(self.ob.rotation_euler[2]),self.ob.location[0]+distanceX*np.sin(self.ob.rotation_euler[2])])
+        self.lineSensor.append([(self.ob.location[0]+distanceX*np.cos(self.ob.rotation_euler[2]))*np.cos(-angle1),(self.ob.location[0]+distanceX*np.sin(self.ob.rotation_euler[2]))*np.sin(-angle1)])
+        self.lineSensor.append([(self.ob.location[0]+distanceX*np.cos(self.ob.rotation_euler[2]))*np.cos(-angle2),(self.ob.location[0]+distanceX*np.sin(self.ob.rotation_euler[2]))*np.sin(-angle2)])
+        self.distSensor = [self.ob.location[0]+distanceXDist*np.cos(self.ob.rotation_euler[2]),self.ob.location[0]+distanceXDist*np.sin(self.ob.rotation_euler[2])])
--- test_thierry.py
+++ test_thierry.py
@@ -3,40 +3,44 @@ from bpy import context as C
 from bpy import data as D
 import sys
 import numpy as np
+import addon_utils
+import ObjectCar
 
 FPS = 24
 AI_UPDATE_RATE = 5
 TIME_BETWEEN_AI_UPDATE = AI_UPDATE_RATE/FPS
 
-def turnAngleSpeed(angle, speed, lastFrame, ob):
-    facingAngle = ob.rotation_euler[2]
+def turnAngleSpeed(angle, speed, lastFrame, car):
+    facingAngle = car.facingAngle
     turn = [facingAngle, facingAngle + angle]
     frames = [lastFrame,  lastFrame + AI_UPDATE_RATE]
     distance = TIME_BETWEEN_AI_UPDATE * speed
-    positionFinalX = [ob.location[0], ob.location[0] + np.cos(facingAngle + angle)*distance]
-    positionFinalY = [ob.location[1], ob.location[1] + np.sin(facingAngle + angle)*distance]
+    print(distance)
+    positionFinalX = [car.positionXYZ[0], car.positionXYZ[0] + np.cos(facingAngle + angle)*distance]
+    positionFinalY = [car.positionXYZ[1],car.positionXYZ[1] + np.sin(facingAngle + angle)*distance]
     
     for i in range(len(frames)):
         frame = frames[i]
         bpy.context.scene.frame_set(frame)
-        ob.location[0] = positionFinalX[i]
-        ob.location[1] = positionFinalY[i]
-        ob.rotation_euler[2] = turn[i]
+        print(positionFinalX)
+        print(positionFinalY)
+        car.positionXYZ[0] = positionFinalX[i]
+        car.positionXYZ[1] = positionFinalY[i]
+        car.facingAngle = turn[i]
+        car.updateOb()
         bpy.ops.anim.keyframe_insert(type='LocRotScale')
+        print("position des sensors:" + str(car.lineSensor))
     return lastFrame + AI_UPDATE_RATE
+    
 if __name__ == "__main__":
     lastFrame = 1
     ob = bpy.data.objects["Voiture"]
-    ob.rotation_euler[2] = 0 
-    ob.location[0] = 0
-    ob.location[1] = 0
-    ob.location[2] = 0.385
-    for i in range(10):
-        lastFrame = turnAngleSpeed(np.pi/10, 10, lastFrame, ob)
-    for i in range(10):
-        lastFrame = turnAngleSpeed(-np.pi/10, 10, lastFrame, ob)
-    for i in range(10):
-        lastFrame = turnAngleSpeed(-np.pi/10, 10, lastFrame, ob)
-    for i in range(10):
-        lastFrame = turnAngleSpeed(np.pi/10, 10, lastFrame, ob)
+    car = ObjectCar.ObjectCar(ob)
     
+    car.positionXYZ = [0,0,0.385]
+    car.facingAngle = 0
+    car.updateOb()
+    for i in range(0,50):
+        lastFrame = turnAngleSpeed(0,10,lastFrame,car)
+        
+        
