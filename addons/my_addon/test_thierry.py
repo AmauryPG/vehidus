3dd37eaf20b06e03eb8e42415ca0854b0b4323f0
Will <plaw2901@usherbrooke.ca>
2021-11-08 19:20:05 -0500

    Salut, jeune papa celibataire ici

--- addons/my_addon/test_thierry.py
+++ addons/my_addon/test_thierry.py
@@ -4,27 +4,37 @@ from bpy import data as D
 import sys
 import numpy as np
 import addon_utils
+import bmesh
+from mathutils.bvhtree import BVHTree
 
-FPS = 24
-AI_UPDATE_RATE = 1
-TIME_BETWEEN_AI_UPDATE = AI_UPDATE_RATE/FPS
-DISTANCE_X_CAPTEUR_LIGNE = 1.357
+FPS = 24 # frame/seconds
+AI_UPDATE_RATE = 5 #frame
+TIME_BETWEEN_AI_UPDATE = AI_UPDATE_RATE/FPS # seconds
+DISTANCE_X_CAPTEUR_LIGNE = 1.41
 DISTANCE_Y_CAPTEUR_LIGNE = 0.18
-DISTANCE_X_CAPTEUR_DISTANCE = 14.82
+DISTANCE_X_CAPTEUR_DISTANCE = 1.212
+DISTANCE_Z_CAPTEUR_DISTANCE = 0.225
+
 
 class ObjectCar :
 
-    def __init__(self, ob, lineSensorsArray):
+    def __init__(self, ob, lineSensorsArray,distSensor,path,obstacles):
         self.ob = ob
         self.lineSensorsArray = lineSensorsArray
+        self.distSensor = distSensor
         self.facingAngle =  ob.rotation_euler[2]
         self.positionXYZ = [ob.location[0],ob.location[1],ob.location[2]]
         self.speed = 0
+        self.path = path
+        self.obstacles = obstacles
+        self.sensorsValues = [0,0,0,0,0]
+        self.sensorDist = 0
         
     def updateOb(self, frame):
         self.ob.location = [self.positionXYZ[0], self.positionXYZ[1], self.positionXYZ[2]]
         self.ob.rotation_euler[2] = self.facingAngle 
         self.updateSensorPos(frame)
+        return self.sensorsValues, self.sensorDist
         
     def updateSensorPos(self, frame):
         # Sensor Ligne
@@ -47,33 +57,105 @@ class ObjectCar :
             cube.rotation_euler[2] = self.ob.rotation_euler[2]
             cube.keyframe_insert(data_path="location", frame=frame)
             cube.keyframe_insert(data_path="rotation_euler", frame=frame)
+            
         #Sensor distance
-        self.distSensor = [self.ob.location[0]+DISTANCE_X_CAPTEUR_DISTANCE*np.cos(self.ob.rotation_euler[2]),
-                        self.ob.location[0]+DISTANCE_X_CAPTEUR_DISTANCE*np.sin(self.ob.rotation_euler[2])]
+        self.distSensor.location = [(self.ob.location[0]+DISTANCE_X_CAPTEUR_DISTANCE*np.cos(self.ob.rotation_euler[2])),
+                                self.ob.location[1]+DISTANCE_X_CAPTEUR_DISTANCE*np.sin(self.ob.rotation_euler[2]),self.ob.location[2] +
+                                DISTANCE_Z_CAPTEUR_DISTANCE]
+        self.distSensor.rotation_euler[1] = 90
+        self.distSensor.rotation_euler[2] = self.ob.rotation_euler[2]
+        self.distSensor.keyframe_insert(data_path="location", frame=frame)
+        self.distSensor.keyframe_insert(data_path="rotation_euler", frame=frame)
         
         
-
-def turnAngleSpeed(angle, speed, lastFrame, car, path):
+        self.sensorsValues = intersection_check(self.lineSensorsArray,self.path)
+        self.sensorDist =  distance_check(self.distSensor,self.obstacles)
+        
+def turnAngleSpeed(angle, speed, lastFrame, car):
     facingAngle = car.facingAngle
     turn = [facingAngle, facingAngle + angle]
     frames = [lastFrame,  lastFrame + AI_UPDATE_RATE]
-    distance = TIME_BETWEEN_AI_UPDATE * speed
+    distance = (TIME_BETWEEN_AI_UPDATE) * speed
     positionFinalX = [car.positionXYZ[0], car.positionXYZ[0] + np.cos(facingAngle + angle)*distance]
     positionFinalY = [car.positionXYZ[1],car.positionXYZ[1] + np.sin(facingAngle + angle)*distance]
-    
+    sensors = []
     for i in range(len(frames)):
         frame = frames[i]
         bpy.context.scene.frame_set(frame)
         car.positionXYZ[0] = positionFinalX[i]
         car.positionXYZ[1] = positionFinalY[i]
         car.facingAngle = turn[i]
-        car.updateOb(frame)
-        print("S0 " + str(car.lineSensorsArray[0].collides(path)) + "S1 " + str(car.lineSensorsArray[1].collides(path))  + "S2 " + str(car.lineSensorsArray[2].collides(path)) + "S3 " + str(car.lineSensorsArray[3].collides(path)) + "S4 " + str(car.lineSensorsArray[4].collides(path)))
+        sensorsValues, distValue = car.updateOb(frame)
         car.ob.keyframe_insert(data_path="location", frame=frame)
         car.ob.keyframe_insert(data_path="rotation_euler", frame=frame)
-    return lastFrame + AI_UPDATE_RATE
+    return lastFrame + AI_UPDATE_RATE, sensorsValues, distValue
 
-    
+def intersection_check(sensorList,path):
+    scene =  bpy.context.scene
+    result = []
+    #check every object for intersection with every other object
+    for sensor in sensorList:
+            #create bmesh objects
+            bm1 = bmesh.new()
+            bm2 = bmesh.new()
+
+            #fill bmesh data from objects
+            bm1.from_mesh(scene.objects[sensor.name].data)
+            bm2.from_mesh(scene.objects[path.name].data)            
+
+            #fixed it here:
+            bm1.transform(scene.objects[sensor.name].matrix_world)
+            bm2.transform(scene.objects[path.name].matrix_world) 
+
+            #make BVH tree from BMesh of objects
+            obj_now_BVHtree = BVHTree.FromBMesh(bm1)
+            obj_next_BVHtree = BVHTree.FromBMesh(bm2)           
+
+            #get intersecting pairs
+            inter = obj_now_BVHtree.overlap(obj_next_BVHtree)
+
+            #if list is empty, no objects are touching
+            if inter != []:
+                #print(sensor.name + " and " + path.name +" are intersecting")
+                result.append(1)
+            else:
+                #print(sensor.name + " and " + path.name + " are not intersecting")
+                result.append(0)
+    return result
+
+def distance_check(sensorDist,Obstacles):
+    scene =  bpy.context.scene
+    Distance = 0.0
+    #check every object for intersection with every other object
+    for Obj in Obstacles:
+            #create bmesh objects
+            bm1 = bmesh.new()
+            bm2 = bmesh.new()
+
+            #fill bmesh data from objects
+            bm1.from_mesh(scene.objects[Obj.name].data)
+            bm2.from_mesh(scene.objects[sensorDist.name].data)            
+
+            #fixed it here:
+            bm1.transform(scene.objects[Obj.name].matrix_world)
+            bm2.transform(scene.objects[sensorDist.name].matrix_world) 
+
+            #make BVH tree from BMesh of objects
+            obj_now_BVHtree = BVHTree.FromBMesh(bm1)
+            obj_next_BVHtree = BVHTree.FromBMesh(bm2)           
+            #get intersecting pairs
+            inter = obj_now_BVHtree.overlap(obj_next_BVHtree)
+            #if list is empty, no objects are touching
+            if inter != []:
+                xdist = Obs.location[0] - sensorDist[0]
+                ydist = Obs.location[1] - sensorDist[1]
+                Distance = np.sqrt(xdist**2 + ydist**2)
+                print("Car seeing " + Obj + " at " + Distance)
+            else:
+                #print(sensor.name + " and " + path.name + " are not intersecting")
+                Distance = 100000
+                print("Can't see shit")
+    return Distance
     
     
 if __name__ == "__main__":
@@ -85,17 +167,18 @@ if __name__ == "__main__":
     for i in range(5):
         lineSensorsArray[i] = bpy.data.objects["SensorLine" + str(i)]
         lineSensorsArray[i].animation_data_clear()
-    car = ObjectCar(ob,lineSensorsArray)
+        
+    distSensor = bpy.data.objects["SensorDist"]
+    distSensor.rotation_euler[0] = 0
+    distSensor.rotation_euler[1] = 90
+    distSensor.rotation_euler[2] = 0
+    distSensor.animation_data_clear()
+    obstacles = [bpy.data.objects["Cube"], bpy.data.objects["Cube.001"]]
+    car = ObjectCar(ob,lineSensorsArray,distSensor,path,obstacles)
     car.positionXYZ = [0,0,0.385]
     car.facingAngle = 0
     car.updateOb(lastFrame)
-    for i in range(0,40):
-        lastFrame = turnAngleSpeed(0,2,lastFrame,car, path)
-    for i in range(0,10):
-        lastFrame = turnAngleSpeed(np.pi/40, 2,lastFrame,car, path)
-    for i in range(0,40):
-        lastFrame = turnAngleSpeed(0, 2,lastFrame,car, path)
-    for i in range(0,10):
-        lastFrame = turnAngleSpeed(-np.pi/40, 2,lastFrame,car, path)
-        
-    print("position de la voiture:" + str(car.positionXYZ))
+    for i in range(0,30):
+        lastFrame,sensorsValues,distValue = turnAngleSpeed(np.pi/10,2,lastFrame,car)
+        print(sensorsValues)
+        print(distValue)
