b88b7f171f3a0016eed2d5335af712a8e59000b1
Amaury <amaury.palao@outlook.com>
2021-11-09 20:51:45 -0500

    Debut du contournement

--- AI/AI.py
+++ AI/AI.py
@@ -1,9 +1,22 @@
 import numpy as np
 import math
 
+#en cm
+maxCaptUltra = 100
+#en m/s
+maxSpeed = 100
+#en rad
+maxAngle = np.pi
+
+#état de la machine
+LINE_FOLLOWER            = 0
+DODGE_OBJECT_ANALYZE     = 1
+DODGE_OBJECT_EDGE_UP     = 2
+DODGE_OBJECT_EDGE_DOWN   = 3
+DODGE_OBJECT_RETURN_LINE = 4
+
 class AI:
     def __init__(self, speed):
-        self.maxSpeed = float(100)
         # m/s
         self.speed = float(speed)
         # rad -pi à pi
@@ -13,6 +26,9 @@ class AI:
         #le buffer existe dans le cas si le robot ne commence pas totalement au centre de la ligne
         #pour qu'il puisse prendre connaissance de la ligne au début
         self.buffer = 4
+        #mesure en cm
+        self.treshold = 10
+        self.state = LINE_FOLLOWER
 
     #fonction principal de l'AI
     def updateAI(self, captLine, captUltra):
@@ -31,55 +47,28 @@ class AI:
     def setAngle(self, angle):
         self.angle = angle
 
-    #Trouve le point moyen de la ligne sur le sol
-    def averageLine(self, captLine):
-        denum = captLine[0] + captLine[1]+ captLine[2]+ captLine[3]+ captLine[4]
-
-        #s'assure qu'il n'y a pas de division par 0
-        if denum == 0:
-            denum = 1
-
-        return (captLine[0] + 2 * captLine[1]+ 3 * captLine[2]+ 4 * captLine[3]+ 5 * captLine[4])/denum
-
-    #fonction principal pour suivre la ligne
-    def lineFollower(self, captLine, speed):
-        #va chercher la valeur moyenne de l'array de la ligne actuel
-        currLine = self.averageLine(captLine)
-        #va chercher la valeur moyenne de l'array de la ligne précedente
-        prevLine = self.averageLine(self.prevCaptLine)
-        #trouve la difference entre la ligne précedente et la ligne suivante.
-        #i.e. la pente de la tangente
-        deltaLine = (currLine - prevLine)
-
-        self.prevCaptLine = captLine
-
-        #si la ligne est continue alors on garde le même capt
-        if deltaLine != 0 :
-            #fait un régle de trois pour normaliser la vitesse selon la vitesse selectionné
-            speedModifier = speed / 100 * deltaLine
-
-            if self.buffer > 0 :
-                self.speed += round(speedModifier, 2)
-            if self.speed > self.maxSpeed:
-                self.speed = self.maxSpeed
-
-            #calcule l'angle de correction a ajouter
-            angleModifier = np.tan([1 / deltaLine])
-
-            #transforme la valeur de correction négative si on va à gauche
-            if deltaLine < 0:
-                angleModifier = -np.abs(angleModifier[0])
-            else :
-                angleModifier = angleModifier[0]
-
-            if self.buffer > 0 :
-                self.angle += angleModifier
-                #modulo considérant les nombres négative
-                #arrondi l'angle pour réduire un possible surcorrection de l'angle
-                self.angle = round(self.angle - np.pi * math.trunc(self.angle / np.pi), 2)
+    def dodgeObject(self, captUltra):
+        if self.state == DODGE_OBJECT_ANALYZE:
+            if captUltra < maxCaptUltra:
+                #tourne à droite de pi/20
+                self.angle += 0.157
+                self.speed = maxSpeed
             else:
-                self.buffer -= 1
-        else:
-            self.speed = speed
-            #peut être cette ligne (85) n'est pas nécessaire
-            self.angle = 0
+                #changer d'état lorsqu'on est a la limite de l'objet a contourner
+                self.state = DODGE_OBJECT_EDGE_DOWN
+                self.angle = 0
+        elif self.state == DODGE_OBJECT_EDGE_UP:
+            self.angle = maxAngle
+            self.speed = 0.3 * self.speed
+        elif self.state == DODGE_OBJECT_EDGE_DOWN:
+            if captUltra > maxCaptUltra:
+                #la voiture a fini de contourner l'objet
+                self.state = DODGE_OBJECT_RETURN_LINE
+            else:
+                self.angle = -maxAngle
+                self.speed = 0.3 * self.speed
+        elif self.state == DODGE_OBJECT_RETURN_LINE:
+            self.angle = -maxAngle
+            self.speed = 0.3 * self.speed
+
+
