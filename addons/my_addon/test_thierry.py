1cf48b1bc36646ded3d2ebf8f4fe3d62b9788e08
Amaury <amaury.palao@outlook.com>
2021-11-18 17:01:52 -0500

    Manque a faire les virages secs

--- addons/my_addon/test_thierry.py
+++ addons/my_addon/test_thierry.py
@@ -19,10 +19,11 @@ class AI:
         # rad -pi à pi
         self.angle = 0
 
-        self.prevCaptLine = np.array([0, 0, 1, 0, 0])
+        self.prevCaptLine = 3
         #le buffer existe dans le cas si le robot ne commence pas totalement au centre de la ligne
         #pour qu'il puisse prendre connaissance de la ligne au début
         self.buffer = 4
+        self.angleModifier = 0
 
     #fonction principal de l'AI
     def updateAI(self, captLine, captUltra):
@@ -47,52 +48,50 @@ class AI:
 
         #s'assure qu'il n'y a pas de division par 0
         if denum == 0:
-            denum = 1
+            #cas ou il n'y a pas de limite
+            return 3
 
         return (captLine[0] + 2 * captLine[1]+ 3 * captLine[2]+ 4 * captLine[3]+ 5 * captLine[4])/denum
 
     #fonction principal pour suivre la ligne
-    def lineFollower(self, captLine, speed):
+    def lineFollower(self, captLine, speed): 
         #va chercher la valeur moyenne de l'array de la ligne actuel
         currLine = self.averageLine(captLine)
         #va chercher la valeur moyenne de l'array de la ligne précedente
-        prevLine = self.averageLine(self.prevCaptLine)
+        #prevLine = self.averageLine(self.prevCaptLine)
         #trouve la difference entre la ligne précedente et la ligne suivante.
         #i.e. la pente de la tangente
-        deltaLine = (currLine - prevLine)
-
-        self.prevCaptLine = captLine
-
-        #si la ligne est continue alors on garde le même capt
-        if deltaLine != 0 :
-            #fait un régle de trois pour normaliser la vitesse selon la vitesse selectionné
-            speedModifier = speed / 100 * deltaLine
-
-            if self.buffer > 0 :
-                self.speed += round(speedModifier, 2)
-            if self.speed > self.maxSpeed:
-                self.speed = self.maxSpeed
-
-            #calcule l'angle de correction a ajouter
-            angleModifier = np.tan([1 / deltaLine])
-
-            #transforme la valeur de correction négative si on va à gauche
+        deltaLine = (currLine - self.prevCaptLine)
+        
+        if deltaLine == 0:
+            self.speed = speed 
+            self.angle = self.angle + self.angleModifier
+            self.angleModifier = self.angleModifier * (1/20)
+        else:                
+            #0.0558 : 3.2
+            #0.0175 : 1
+            self.angleModifier = np.tan(0.0175/deltaLine)
+            self.angle = self.angle * (1/2) - self.angleModifier
+            
             if deltaLine < 0:
-                angleModifier = -np.abs(angleModifier[0])
-            else :
-                angleModifier = angleModifier[0]
-
-            if self.buffer > 0 :
-                #self.angle += angleModifier
-                #modulo considérant les nombres négative
-                #arrondi l'angle pour réduire un possible surcorrection de l'angle
-                self.angle = round(np.pi * math.trunc(angleModifier / np.pi), 2)
+                self.angle = abs(self.angle)
             else:
-                self.buffer -= 1
-        else:
-            self.speed = speed
-            #peut être cette ligne (85) n'est pas nécessaire
-            self.angle = 0
+                self.angle = -abs(self.angle)
+                
+            if abs(self.angle) > 0:
+                g = abs(2 / (abs(self.angle) + 1) - 1)
+            else :
+                g = 1
+                
+            if abs(self.angle) >= np.pi:
+                h = -1
+            else:
+                h = 1
+                
+            self.speed = self.speed * g * h
+            print("angle ", self.angle)
+            print("angleMod ", self.angleModifier)
+ 
 
 FPS = 24 # frame/seconds
 AI_UPDATE_RATE = 1 #frame
@@ -277,6 +276,6 @@ if __name__ == "__main__":
         lastFrame, sensorsValues, distValue = turnAngleSpeed(ai.getAngle(), ai.getSpeed(), lastFrame, car)
 
     
-    for i in range(400):
-        lastFrame, sensorsValues, distValue = turnAngleSpeed(ai.getAngle()/(20), ai.getSpeed(), lastFrame, car)
+    for i in range(800):
+        lastFrame, sensorsValues, distValue = turnAngleSpeed(ai.getAngle(), ai.getSpeed(), lastFrame, car)
         ai.lineFollower(sensorsValues, 2)
